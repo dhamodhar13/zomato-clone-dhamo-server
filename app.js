557f41a4b4343f9fb9cd551dd6a14f4345924280
dhamodhar13 <dhamodhar13@hotmail.com>
2021-09-14 13:55:13 +0530

    server

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const appRouter = require('./router/index')
+
+const port = process.env.PORT || 5000;
+const host = '0.0.0.0';
+
+const app = express();
+
+app.use(cors());
+app.options('*', cors());
+
+app.use(express.json());
+app.use('/', appRouter);
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(
+    app.listen(port, host)
+).catch(err => console.log(err));
+
