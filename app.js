a709f5b2dcff185c2d5c51af125beb1f7c774a42
dhamodhar13 <dhamodhar13@hotmail.com>
2021-09-15 09:39:47 +0530

    cors issue

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ const port = process.env.PORT || 5000;
 const host = '0.0.0.0';
 
 const app = express();
-
+app.use(express.json());
 app.use(cors());
 app.options('*', cors());
-app.use(express.json());
+
 app.use('/', appRouter);
 
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = procecss.env.DB_PASSWORD;
+// const DB_USER = process.env.DB_USER;
+// const DB_PASSWORD = procecss.env.DB_PASSWORD;
 
-mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@edureka-fullstack-proje.nq1mx.mongodb.net/database0?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(`mongodb+srv://user00:GTJzQ5SGNlO43Tyi@edureka-fullstack-proje.nq1mx.mongodb.net/database0?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
     .then(
     app.listen(port, host)
 ).catch(err => console.log(err));
