1e3ea15986bb93c5fad3b61d5807feb6fd127abb
dhamodhar13 <dhamodhar13@hotmail.com>
2022-03-01 11:38:31 +0530

    Optimized

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const appRouter = require('./router/index')
+const appRouter = require("./router/index");
 
 const port = process.env.PORT || 5000;
-const host = '0.0.0.0';
+const host = "0.0.0.0";
 
 const app = express();
 
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 app.use(express.json());
-app.use('/', appRouter);
+app.use("/", appRouter);
 
 const dbUrl = process.env.DB_URL;
-mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(
-    app.listen(port, host)
-).catch(err => console.log(err));
-
+mongoose
+  .connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(app.listen(port, host))
+  .catch((err) => console.log(err));
